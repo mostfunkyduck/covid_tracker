2754336fd42955ae5bf0e846dac72da2af11dd1f
funkyduck <e@m.ail>
2020-05-11 04:23:19 -0400

    adding worldometer

--- main.py
+++ main.py
@@ -4,17 +4,41 @@ import json
 import time
 
 class Data:
+    gauges = {
+        "confirmed": Gauge("covid_tracker_confirmed_total", "confirmed cases", ["country", "province", "source"]),
+        "deaths": Gauge("covid_tracker_deaths_total", "total deaths", ["country", "province", "source"]),
+        "recovered": Gauge("covid_tracker_recovered_total", "total recovered", ["country", "province", "source"])
+    }
     def pull(self):
         return requests.get(self.url)
 
+class WorldometerWorldData (Data):
+    def __init__(self, **kwargs):
+        super(WorldometerWorldData, self).__init__(**kwargs)
+        self.url = "https://disease.sh/v2/countries"
+
+    def process(self):
+        json = self.pull().json()
+        for unit in json:
+            self.gauges["deaths"].labels(country=unit["country"], province="all", source="worldometer").set(unit["deaths"])
+            self.gauges["confirmed"].labels(country=unit["country"], province="all", source="worldometer").set(unit["cases"])
+            self.gauges["recovered"].labels(country=unit["country"], province="all", source="worldometer").set(unit["recovered"])
+
+class WorldometerUSData (Data):
+    def __init__(self, **kwargs):
+        super(WorldometerUSData, self).__init__(**kwargs)
+        self.url = "https://disease.sh/v2/states"
+
+    def process(self):
+        json = self.pull().json()
+        for unit in json:
+            self.gauges["deaths"].labels(country="USA", province=unit["state"], source="worldometer").set(unit["deaths"])
+            self.gauges["confirmed"].labels(country="USA", province=unit["state"], source="worldometer").set(unit["cases"])
+
 class JHU (Data):
     def __init__(self, **kwargs):
+        super(JHU,self).__init__(**kwargs)
         self.url = "https://disease.sh/v2/jhucsse"
-        self.gauges = {
-            "confirmed": Gauge("covid_tracker_confirmed_total", "confirmed cases", ["country", "province", "source"]),
-            "deaths": Gauge("covid_tracker_deaths_total", "total deaths", ["country", "province", "source"]),
-            "recovered": Gauge("covid_tracker_recovered_total", "total recovered", ["country", "province", "source"])
-        }
 
     def process(self):
         json = self.pull().json()
@@ -27,8 +51,11 @@ def main():
     
     start_http_server(9002)
     jhu = JHU()
+    w = WorldometerWorldData()
+    gatherers = [JHU(), WorldometerWorldData(), WorldometerUSData()]
     while True:
-        jhu.process()
+        for g in gatherers:
+            g.process()
         print("processed chunk")
         # this data only updates every 10 minutes, no point in spamming the API
         time.sleep(600)        
